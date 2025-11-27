f526b454e8ebeb5e0f397dc6a937787cd97a5301
Zofia Nora <sophienora92@gmail.com>
2025-11-27 18:59:07 +0100

    deleted one font

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist,} from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -7,10 +7,7 @@ const geistSans = Geist({
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
 
 export const metadata: Metadata = {
   title: "CycleCoach",
