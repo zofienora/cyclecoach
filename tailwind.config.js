cb419ba6879bf6f278e0af483bdf7868150806a1
Zofia Nora <sophienora92@gmail.com>
2025-11-28 19:17:15 +0100

    added poppins to tailwind file

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,3 +7,17 @@ module.exports = {
   plugins: [],
 }
 
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-poppins)'],
+      },
+    },
+  },
+  plugins: [],
+}
