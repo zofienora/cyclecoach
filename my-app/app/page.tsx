d4a8cae0ce81ddaf6f420dedef5d0dd100ed6841
Zofia Nora <sophienora92@gmail.com>
2025-11-29 14:24:13 +0100

    added Header to page.tsx

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,8 +1,10 @@
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
-    <div>
- 
-    </div>
+    <main className="max-w-md mx-auto px-4">
+      <Header />
+      {/* More components like PhaseTracker will come next */}
+    </main>
   );
 }
