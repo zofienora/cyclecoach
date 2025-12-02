6315e382e9caade7cb47dfba45c0134add83fcb7
Zofia Nora <sophienora92@gmail.com>
2025-12-02 10:20:30 +0100

    added 90 p width to page

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -2,7 +2,7 @@ import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <main className="max-w-md mx-auto px-4">
+    <main className="w-[90%] max-w-xl mx-auto px-4">
       <Header />
       {/* More components like PhaseTracker will come next */}
     </main>
