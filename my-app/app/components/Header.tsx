e7e50b415fe98f7dfea144a8388025d872c1ee78
Zofia Nora <sophienora92@gmail.com>
2025-12-03 18:40:22 +0100

    font weight

--- my-app/app/components/Header.tsx
+++ my-app/app/components/Header.tsx
@@ -2,12 +2,12 @@
 
 export default function Header() {
   return (
-    <header className="flex flex-col items-center px-4 py-16 mx-auto gap-4">
+    <header className="flex flex-col items-center px-4 pt-4 mx-auto gap-4">
       <div className="flex flex-col items-center ">
-        <h1 className="text-7xl font-heading text-secondary text-centerpy-16 py-8">
+        <h1 className="text-7xl font-bold border font-heading text-secondary text-centerpy-16 py-8">
           CycleCoach
         </h1>
-        <h2 className="font-heading text-foreground text-centerpy-8 text-3xl py-8
+        <h2 className="font-heading text-foreground text-center text-3xl pt-8 pb-2
 ">
           Hello, Sophie 👋
         </h2>
