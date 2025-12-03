be6616c1af1e7060be67a85a81fe4169e3d7e79f
Zofia Nora <sophienora92@gmail.com>
2025-12-03 18:30:35 +0100

    changing text sizes, no problems with tailwind yay

--- my-app/app/components/Header.tsx
+++ my-app/app/components/Header.tsx
@@ -4,10 +4,11 @@ export default function Header() {
   return (
     <header className="flex flex-col items-center px-4 py-6 mx-auto gap-4 border-2">
       <div className="flex flex-col items-center border">
-        <h1 className="text-[48px] font-heading text-secondary text-center border-2">
+        <h1 className="text-7xl font-heading text-secondary text-center border-2 py-16">
           CycleCoach
         </h1>
-        <h2 className="text-lg font-heading text-foreground text-center border-4 py-8">
+        <h2 className="font-heading text-foreground text-center border-4 py-8 text-3xl
+">
           Hello, Sophie 👋
         </h2>
       </div>
