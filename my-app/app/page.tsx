eb3dc9af8e4ca31da26c9dfaeff6c091f18401c6
Zofia Nora <sophienora92@gmail.com>
2025-12-04 11:56:13 +0100

    added phase tracker component to oage

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import PhaseTracker from "./components/PhaseTracker";
 
 export default function Home() {
   return (
     <main className="w-[90%] max-w-xl mx-auto px-4">
       <Header />
+      <PhaseTracker />
       {/* More components like PhaseTracker will come next */}
     </main>
   );
