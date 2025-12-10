4fc2b3d41de77d2a6e1b352659570f42790d5bc0
Zofia Nora <sophienora92@gmail.com>
2025-12-10 18:17:25 +0100

    trying to make the circles stay in the div

--- my-app/app/components/PhaseTracker.tsx
+++ my-app/app/components/PhaseTracker.tsx
@@ -2,6 +2,15 @@
 import * as motion from "motion/react-client"
 
 export default function PhaseTracker() {
+    const containerRef = useRef(null);
+    const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+
+    useEffect(() => {
+    if (containerRef.current) {
+        const rect = containerRef.current.getBoundingClientRect();
+        setDimensions({ width: rect.width, height: rect.height });
+    }
+    }, []);
   return (
    <section className="border-2 border-alert rounded-lg p-4 h-screen">
     <h2 className="text-2xl font-bold font-heading text-foreground text-center py-8">
@@ -10,13 +19,13 @@ export default function PhaseTracker() {
     <h3 className="text-lg font-sans text-foreground text-center py-8">
       calm movement, nourishing food, sleep and relaxation
     </h3>
-    <div className="w-[90%] h-[80%] mx-auto">
+    <div className="w-[90%] h-[80%] mx-auto border relative">
         <motion.div
             className="w-50 h-50 rounded-full bg-secondary absolute"
             animate={{
-            x: [10, 60, -40, 30, -20, 0],
-            y: [0, -30, 50, -10, 40, 0],
-            }}
+                x: [0, dimensions.width * 0.3, 0],
+                y: [0, dimensions.height * 0.5, 0],
+              }}
             transition={{
             repeat: Infinity,
             duration: 12,
@@ -27,9 +36,9 @@ export default function PhaseTracker() {
       <motion.div
         className="w-10 h-10 rounded-full bg-primary absolute"
         animate={{
-          x: [0, 300, -200, 150, -100, 0],
-          y: [0, -200, 200, -150, 100, 0],
-        }}
+            x: [0, dimensions.width * 0.3, 0],
+            y: [0, dimensions.height * 0.5, 0],
+          }}
         transition={{
           repeat: Infinity,
           duration: 100,
@@ -41,9 +50,9 @@ export default function PhaseTracker() {
       <motion.div
         className="w-8 h-8 rounded-full bg-accent absolute"
         animate={{
-          x: [0, 400, -300, 200, -100, 0],
-          y: [0, -100, 100, -50, 50, 0],
-        }}
+            x: [0, dimensions.width * 0.3, 0],
+            y: [0, dimensions.height * 0.5, 0],
+          }}
         transition={{
           repeat: Infinity,
           duration: 14,
