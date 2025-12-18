73c1e78a2c5ec2e8d73f017379e66f6bed73766b
Zofia Nora <sophienora92@gmail.com>
2025-12-18 17:31:19 +0100

    changes

--- my-app/app/components/Suggestions.tsx
+++ my-app/app/components/Suggestions.tsx
@@ -4,17 +4,16 @@
 export default function Suggestions() {
 
   return (
-   <section className="border-2 border-alert rounded-lg p-4 h-screen">
+   <section className="rounded-lg p-4 h-screen">
     <h2 className="text-2xl font-bold font-heading text-foreground text-center py-8">
       Explore
     </h2>
     <div className="">
-        <div className="bg-primary rounded-lg p-4">
-
-
+        <div className="bg-primary rounded-lg p-10 mt-5 mb-5">
+            <p className="text-lg font-bold font-heading text-white uppercase text-center pt-10 pb-10">Upcoming Phase</p>
         </div>
-        <div className="border-2 border-secondary rounded-lg p-4"></div>
-        <div className="border-2 border-alert rounded-lg p-4"></div>
+        <div className="border-2 border-secondary rounded-lg p-10 mt-5 mb-5"></div>
+        <div className="border-2 border-alert rounded-lg p-10 mt-5 mb-5"></div>
     </div>
 
    </section>
